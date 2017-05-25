7a3c17a6b26191df277bd15a42124557bd5bb2a3
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-05-24 23:59:33 -0400

    added functionality to handle mouse clicks

--- src/MatchingGame_script.js
+++ src/MatchingGame_script.js
@@ -1,10 +1,10 @@
 var numberOfFaces = 5;
 var theLeftSide = document.getElementById("leftSide");
 var theRightSide = document.getElementById("rightSide");
-
+var theBody = document.getElementsByTagName("body")[0];
 function generateFaces() {
 
-    while(numberOfFaces--) {
+    for(var count = 0; count < numberOfFaces; count++) {
         var smiley_face = document.createElement("img");
         smiley_face.src = "smile.png";
         smiley_face.style.top = Math.floor(Math.random() * 400);
@@ -12,7 +12,36 @@ function generateFaces() {
         theLeftSide.appendChild(smiley_face);
     }
 
-    var leftSideImages = theLeftSide.cloneNode(true);
-    leftSideImages.removeChild(leftSideImages.lastChild);
-    theRightSide.appendChild(leftSideImages);
+    var rightSideImages = theLeftSide.cloneNode(true);
+    rightSideImages.removeChild(rightSideImages.lastChild);
+    theRightSide.appendChild(rightSideImages);
+
+    theLeftSide.lastChild.onclick =
+        function nextLevel(event){
+            event.stopPropagation();
+            alert("hdsfhljdsfjhkdfjklsdf");
+            numberOfFaces += 5;
+            generateFaces();
+        };
 }
+
+
+theBody.onclick =
+    function gameOver() {
+        alert("Game Over!");
+        theBody.onclick = null;
+        theLeftSide.lastChild.onclick = null;
+    };
+
+// function printChildInfo(parent)
+// {
+//     var childs = parent.childNodes;
+//
+//     for (var i = 0; i < childs.length; ++i)
+//     {
+//         var c = childs[i];
+//
+//         console.log([c.nodeName, c.nodeValue]);
+//     }
+// }
+// printChildInfo(theLeftSide);
